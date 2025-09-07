25ca3209aa145aeb6adeaf6c5d0841755c8675ab
MiniMaliZ <gakisomlebu@gmail.com>
2025-09-07 16:54:46 +0700

    Constanta dan FInal

--- bin/project1.dart
+++ bin/project1.dart
@@ -1,5 +1,10 @@
 import 'package:project1/project1.dart' as project1;
 
+const String namaDepan = 'Sufyan';
+
 void main(List<String> arguments) {
-  print('Hello world: ${project1.calculate()}!');
+  // print('Hello world: ${project1.calculate()}!');
+
+  final String namaBelakang = 'Dwi Bagaskara';
+  print("Halo Nama Saya $namaDepan $namaBelakang ");
 }
