ab96cab072ed040b0abf5755741324bed5dc7ac3
MiniMaliZ <gakisomlebu@gmail.com>
2025-09-07 17:43:30 +0700

    Variable Advanced : List & Map

--- bin/project1.dart
+++ bin/project1.dart
@@ -2,30 +2,48 @@ import 'dart:io';
 
 import 'package:project1/project1.dart' as project1;
 
+// Constanta
 const String namaDepan = 'Sufyan';
 
 void main(List<String> arguments) {
   // print('Hello world: ${project1.calculate()}!');
 
-  // Constanta dan Final
+  // Final
   final String namaBelakang = 'Dwi Bagaskara';
-
-  // print("Halo Nama Saya $namaDepan $namaBelakang ");
 
   // Variable
   int umur = 20;
   double tinggi = 172.5;
   bool jenisKelamin = true; // true = laki-laki, false = perempuan
   String alamat = 'Jl. Anila 3';
+  num nilai = 85; // num bisa menampung int dan double
+  dynamic sakOnoke = 'Variable Bebas';
+  dynamic x = nilai;
 
-  stdout.writeln("Halo Nama Saya $namaDepan $namaBelakang ");
-  stdout.writeln("Umur Saya $umur Tahun");
-  stdout.writeln("Tinggi Saya $tinggi cm");
-  stdout.writeln("Jenis Kelamin ${jenisKelamin ? 'Laki-laki' : 'Perempuan'}");
-  // if (jenisKelamin == true) {
-  //   stdout.writeln("Jenis Kelamin Laki-laki");
-  // } else {
-  //   stdout.writeln("Jenis Kelamin Perempuan");
-  // }
-  stdout.writeln("Alamat Saya $alamat");
+  // List
+  List<String> hobi = ['Membaca', 'Menulis', 'Coding'];
+  hobi.add('Berenang');
+
+  // Map
+  Map<String, String> buku = {
+    'judul': 'Pemrograman Dart',
+    'penulis': 'Sufyan Dwi Bagaskara',
+    'tahun': '2021'
+  };
+  buku['penerbit'] = 'Dicoding';
+  
+  // Print
+  // stdout.writeln("Halo Nama Saya $namaDepan $namaBelakang ");
+  // stdout.writeln("Umur Saya $umur Tahun");
+  // stdout.writeln("Tinggi Saya $tinggi cm");
+  // stdout.writeln("Jenis Kelamin ${jenisKelamin ? 'Laki-laki' : 'Perempuan'}");
+  // stdout.writeln("Alamat Saya $alamat");
+  // stdout.writeln("Nilai Saya $nilai");
+  // stdout.writeln("Iki $sakOnoke");
+  // stdout.writeln("Int ke Dynamic $x");
+  // stdout.writeln("Hobi Saya ${hobi.join(', ')}");
+
+  // Print Map
+  stdout.writeln("Buku : $buku");
+  stdout.writeln("Judul Buku : ${buku['judul']}");
 }
